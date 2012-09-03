1b57e80041a6147f12dd7c5aac7cf2de00d0ff9a
Joel Kirchartz <JKirchartz@gmail.com>
2012-09-03 09:19:59 -0400

    whittle down background js

--- js/background.js
+++ js/background.js
@@ -43,36 +43,6 @@ var mul_table = [ 1,57,41,21,203,34,97,73,227,91,149,62,105,45,39,137,241,107,3,
    
 var shg_table = [0,9,10,10,14,12,14,14,16,15,16,15,16,15,15,17,18,17,12,18,16,17,17,19,19,18,19,18,18,19,19,19,20,19,20,20,20,20,20,20,15,20,19,20,20,20,21,21,21,20,20,20,21,18,21,21,21,21,20,21,17,21,21,21,22,22,21,22,22,21,22,21,19,22,22,19,20,22,22,21,21,21,22,22,22,18,22,22,21,22,22,23,22,20,23,22,22,23,23,21,19,21,21,21,23,23,23,22,23,23,21,23,22,23,18,22,23,20,22,23,23,23,21,22,20,22,21,22,24,24,24,24,24,22,21,24,23,23,24,21,24,23,24,22,24,24,22,24,24,22,23,24,24,24,20,23,22,23,24,24,24,24,24,24,24,23,21,23,22,23,24,24,24,22,24,24,24,23,22,24,24,25,23,25,25,23,24,25,25,24,22,25,25,25,24,23,24,25,25,25,25,25,25,25,25,25,25,25,25,23,25,23,24,25,25,25,25,25,25,25,25,25,24,22,25,25,23,25,25,20,24,25,24,25,25,22,24,25,24,25,24,25,25,24,25,25,25,25,22,25,25,25,24,25,24,25,18];
 
-/*		
-function boxBlurImage( imageID, canvasID, radius, blurAlphaChannel, iterations ){
-			
- 	var img = document.getElementById( imageID );
-	var w = img.naturalWidth;
-    var h = img.naturalHeight;
-       
-	var canvas = document.getElementById( canvasID );
-      
-    canvas.style.width  = w + "px";
-    canvas.style.height = h + "px";
-    canvas.width = w;
-    canvas.height = h;
-    
-    var context = canvas.getContext("2d");
-    context.clearRect( 0, 0, w, h );
-    context.drawImage( img, 0, 0 );
-
-	if ( isNaN(radius) || radius < 1 ) return;
-	
-	if ( blurAlphaChannel )
-	{
-		boxBlurCanvasRGBA( canvasID, 0, 0, w, h, radius, iterations );
-	} else {
-		boxBlurCanvasRGB( canvasID, 0, 0, w, h, radius, iterations );
-	}
-	
-}
-*/
-
 function boxBlurCanvasRGBA( id, top_x, top_y, width, height, radius, iterations ){
 	if ( isNaN(radius) || radius < 1 ) return;
 	
@@ -115,15 +85,15 @@ function boxBlurCanvasRGBA( id, top_x, top_y, width, height, radius, iterations 
 	var rsum,gsum,bsum,asum,x,y,i,p,p1,p2,yp,yi,yw,idx,pa;		
 	var wm = width - 1;
   	var hm = height - 1;
-    var wh = width * height;
+        var wh = width * height;
 	var rad1 = radius + 1;
     
 	var mul_sum = mul_table[radius];
 	var shg_sum = shg_table[radius];
 
 	var r = [];
-    var g = [];
-    var b = [];
+        var g = [];
+        var b = [];
 	var a = [];
 	
 	var vmin = [];
@@ -220,143 +190,6 @@ function boxBlurCanvasRGBA( id, top_x, top_y, width, height, radius, iterations 
 	context.putImageData( imageData, top_x, top_y );
 	
 }
-/*
-function boxBlurCanvasRGB( id, top_x, top_y, width, height, radius, iterations ){
-	if ( isNaN(radius) || radius < 1 ) return;
-	
-	radius |= 0;
-	
-	if ( isNaN(iterations) ) iterations = 1;
-	iterations |= 0;
-	if ( iterations > 3 ) iterations = 3;
-	if ( iterations < 1 ) iterations = 1;
-	
-	var canvas  = document.getElementById( id );
-	var context = canvas.getContext("2d");
-	var imageData;
-	
-	try {
-	  try {
-		imageData = context.getImageData( top_x, top_y, width, height );
-	  } catch(e) {
-	  
-		// NOTE: this part is supposedly only needed if you want to work with local files
-		// so it might be okay to remove the whole try/catch block and just use
-		// imageData = context.getImageData( top_x, top_y, width, height );
-		try {
-			netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
-			imageData = context.getImageData( top_x, top_y, width, height );
-		} catch(e) {
-			alert("Cannot access local image");
-			throw new Error("unable to access local image data: " + e);
-			return;
-		}
-	  }
-	} catch(e) {
-	  alert("Cannot access image");
-	  throw new Error("unable to access image data: " + e);
-	  return;
-	}
-			
-	var pixels = imageData.data;
-		
-	var rsum,gsum,bsum,asum,x,y,i,p,p1,p2,yp,yi,yw,idx;		
-	var wm = width - 1;
-  	var hm = height - 1;
-    var wh = width * height;
-	var rad1 = radius + 1;
-   
-	var r = [];
-    var g = [];
-    var b = [];
-	
-	var mul_sum = mul_table[radius];
-	var shg_sum = shg_table[radius];
-	
-	var vmin = [];
-	var vmax = [];
-  
-	while ( iterations-- > 0 ){
-		yw = yi = 0;
-	 
-		for ( y=0; y < height; y++ ){
-			rsum = pixels[yw]   * rad1;
-			gsum = pixels[yw+1] * rad1;
-			bsum = pixels[yw+2] * rad1;
-			
-			for( i = 1; i <= radius; i++ ){
-				p = yw + (((i > wm ? wm : i )) << 2 );
-				rsum += pixels[p++];
-				gsum += pixels[p++];
-				bsum += pixels[p++];
-			}
-			
-			for ( x = 0; x < width; x++ ){
-				r[yi] = rsum;
-				g[yi] = gsum;
-				b[yi] = bsum;
-				
-				if( y==0) {
-					vmin[x] = ( ( p = x + rad1) < wm ? p : wm ) << 2;
-					vmax[x] = ( ( p = x - radius) > 0 ? p << 2 : 0 );
-				} 
-				
-				p1 = yw + vmin[x];
-				p2 = yw + vmax[x];
-				  
-				rsum += pixels[p1++] - pixels[p2++];
-				gsum += pixels[p1++] - pixels[p2++];
-				bsum += pixels[p1++] - pixels[p2++];
-				 
-				yi++;
-			}
-			yw += ( width << 2 );
-		}
-	  
-		for ( x = 0; x < width; x++ ){
-			yp = x;
-			rsum = r[yp] * rad1;
-			gsum = g[yp] * rad1;
-			bsum = b[yp] * rad1;
-				
-			for( i = 1; i <= radius; i++ ){
-			  yp += ( i > hm ? 0 : width );
-			  rsum += r[yp];
-			  gsum += g[yp];
-			  bsum += b[yp];
-			}
-			
-			yi = x << 2;
-			for ( y = 0; y < height; y++){
-				pixels[yi]   = (rsum * mul_sum) >>> shg_sum;
-				pixels[yi+1] = (gsum * mul_sum) >>> shg_sum;
-				pixels[yi+2] = (bsum * mul_sum) >>> shg_sum;
-		   
-				if( x == 0 ) {
-					vmin[y] = ( ( p = y + rad1) < hm ? p : hm ) * width;
-					vmax[y] = ( ( p = y - radius) > 0 ? p * width : 0 );
-				} 
-				  
-				p1 = x + vmin[y];
-				p2 = x + vmax[y];
-
-				rsum += r[p1] - r[p2];
-				gsum += g[p1] - g[p2];
-				bsum += b[p1] - b[p2];
-				  
-				yi += width << 2;
-			}
-		}
-	}
-	context.putImageData( imageData, top_x, top_y );
-	
-}
-
-
-*/
-
-
-
 
 
 /*********************************************
